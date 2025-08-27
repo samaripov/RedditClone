fdbda58fe22ec42de4df90e07c3b044e66c14255
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-27 12:48:44 -0400

    Removed the validation for the description.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 70 }
-  validates :description, length: { minimum: 3,  maximum: 70 }
 end
