77554e6a4168adb315852c4447591bcfc64e3a5b
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-27 15:26:43 -0400

    Fixed post creation form bug where back rendered content missing. Added an image upload ability.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :post_image
   validates :title, presence: true, length: { minimum: 3, maximum: 70 }
   validates :description, length: { maximum: 400 }
 end
