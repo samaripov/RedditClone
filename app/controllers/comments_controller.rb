980993743a9d478764d6396e65e07c9a42f70b51
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-09-05 21:20:14 -0400

    Posting boiler plate comments atm.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,25 @@
 class CommentsController < ApplicationController
   before_action :require_login
+  before_action :set_post
   def new
-    post = Post.find(params[:id])
-    @comment = current_user.comments.build(post: post)
+    @comment = current_user.comments.build(post: @post)
   end
 
   def create
+    @comment = current_user.comments.build(**comment_params, **{ post: @post })
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("comments-for-post-#{@post.id}", partial: "comments/comment", locals: { comment: @comment })
+        end
+      else
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace("errors_div", partial: "layouts/errors", locals: { entity: @comment })
+            ], status: :unprocessable_entity
+        end
+      end
+    end
   end
 
   def edit
@@ -18,9 +32,16 @@ class CommentsController < ApplicationController
   end
 
   private
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
     def require_login
       unless current_user
         redirect_to new_session_path
       end
+    end
+
+    def comment_params
+      params.require(:comment).permit(:content)
     end
 end
