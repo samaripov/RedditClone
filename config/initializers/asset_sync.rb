571528e666f4823b60cc5e55c34bb8a9669c7bc3
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-09-16 21:52:28 -0400

    Updated the S3 initializer

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -5,6 +5,7 @@ if defined?(AssetSync)
     config.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
     config.fog_directory = "campfire-storage-bucket"
     config.fog_region = "us-east-2"
+    config.fog_public = true
     config.existing_remote_files = "delete" # or 'keep'
     config.gzip_compression = true
   end
